86f55d76bb08cbfe9324c789fa1ff6616153e2a0
salpreh <salva.perez46@gmail.com>
2023-06-11 22:41:31 +0200

    feat: Hibernate persistence config

--- src/main/java/com/salpreh/products/persistence/entities/ProductEntity.java
+++ src/main/java/com/salpreh/products/persistence/entities/ProductEntity.java
@@ -1,15 +1,28 @@
 package com.salpreh.products.persistence.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "product")
 public class ProductEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_seq")
+  @SequenceGenerator(name = "product_id_seq", allocationSize = 1)
   private Long id;
   private String name;
   private String description;
